2e2c72c4b47488c355ed5d58e45d09f303ff19cb
JoseLuisADA <joseluis.luzuriagaalanis.web@adaits.es>
2023-09-25 17:33:54 +0200

    EJERCICIO 5 COMPLETADO SPRING 1

---
+++ T1/SPRINT 1/recursos/scripts/ejercicio5.js
@@ -0,0 +1,11 @@
+console.log("Números del 1 al 10 :");
+
+for(let numero = 1; numero <= 10; numero++){
+    console.log(numero);
+}
+
+console.log("Números del 10 al 1 :")
+
+for(let numero = 10; numero >= 0; numero--){
+    console.log(numero);
+}
