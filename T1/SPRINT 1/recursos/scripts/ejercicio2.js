825108653cfb4144f360cd9378dfb920945457e0
Jose Luis Luzuriaga Alanis <joseluis.luzuriagaalanis.web@adaits.es>
2023-09-25 14:30:04 +0200

    EJERCICIO 2 COMPLETADO Y EJERCICIO 3  SIN TERMINAR

---
+++ T1/SPRINT 1/recursos/scripts/ejercicio2.js
@@ -0,0 +1,8 @@
+let operando1 = 2;
+let operando2 = 1;
+
+console.log("La suma de "+ operando1 + " y " + operando2 + " es " + (operando1+operando2));
+console.log("La resta de "+ operando1 + " y " + operando2 + " es " + (operando1-operando2));
+console.log("La multiplicación de "+ operando1 + " y " + operando2 + " es " + (operando1*operando2));
+console.log("La división de "+ operando1 + " entre " + operando2 + " es " + (operando1/operando2));
+console.log("El módulo de "+ operando1 + " entre " + operando2 + " es " + (operando1+operando2));
---
+++ T1/SPRINT 1/recursos/scripts/ejercicio3.js
@@ -0,0 +1,16 @@
+let A = 1;
+let B = "1";
+let C = 3;
+let verdaderoFalso;
+
+verdaderoFalso = A == B;
+console.log("1 == 1 es " + verdaderoFalso + " porque 1 es igual 1");
+
+verdaderoFalso = A === B;
+console.log("1 === 1 es " + verdaderoFalso + " porque 1 es igual 1");
+
+verdaderoFalso = A > C;
+console.log("1 > 3 es " + verdaderoFalso);
+
+verdaderoFalso = A < B;
+console.log("1  2 es " + verdaderoFalso);
