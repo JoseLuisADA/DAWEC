25133c5d303d3c5cc96127d13be49e5fffc81777
JoseLuisADA <joseluis.luzuriagaalanis.web@adaits.es>
2023-09-25 17:09:38 +0200

    EJERCICIO 4 COMPLETADO SPRING 1

---
+++ T1/SPRINT 1/recursos/scripts/ejercicio4.js
@@ -0,0 +1,9 @@
+let numero = prompt("Vamos a comprobar si funciona el comparador de mayor, menor e igual, teniendo como referencia el número 10. Introduce un número :");
+
+if(numero > 10){
+    alert("¡El número es mayor que 10!");
+} else if(numero < 10){
+    alert("¡El número es menor que 10!");
+} else if(numero == 10){
+    alert("¡Los numeros son iguales!");
+}
