963ad4f2ea7541dfb794875502efdc8a4a0b0b19
Jose Luis Luzuriaga Alanis <joseluis.luzuriagaalanis.web@adaits.es>
2023-09-25 12:53:35 +0200

    Ejercicio 1

---
+++ T1/SPRINT 1/recursos/scripts/ejercicio1.js
@@ -0,0 +1,13 @@
+let numero = 2;
+let texto = "Hola";
+let verdaderoFalso = true;
+let persona = {nombre: "Jose Luis", apellidos: "Luzuriaga Alanis"};
+let nulo = null;
+let sinDefinir;
+
+console.log(numero + " es de tipo " + typeof numero);
+console.log(texto + " es de tipo " + typeof texto);
+console.log(verdaderoFalso + " es de tipo " + typeof verdaderoFalso);
+console.log(persona.nombre + " " + persona.apellidos + " es de tipo " + typeof persona);
+console.log(nulo + " es de tipo " + typeof nulo);
+console.log(sinDefinir + " es de tipo " + typeof sinDefinir);
